9402274db3980ca179186df82343e99992b8b90c
alvinsj <email.to.alvin@gmail.com>
2015-11-11 05:32:47 +0700

    remove instagram-node

--- js/actions/app-server-actions.js
+++ js/actions/app-server-actions.js
@@ -1,8 +1,6 @@
 var AppDispatcher = require('dispatchers/app-dispatcher');
 var ActionTypes = require('constants/app-constants').ActionTypes;
 var StateStore = require('stores/state-store'),
-
-    ig = StateStore.ig,
     localForage = require('localForage');
 
 var _redirect_url = "http://alvinsj.com/viewfinder/ffos";
@@ -70,74 +68,74 @@ module.exports = {
         };
         browser.addEventListener('mozbrowserlocationchange', dispatch);
     },
-    getInstagramCode: function(){
-        console.log('getInstagramCode')
-
-        StateStore.ig = require('instagram-node').instagram();
-        ig = StateStore.ig;
-        ig.use(require('instagram'));
-
-        var auth_url = ig.get_authorization_url(_redirect_url);
-        var browser = document.getElementById('browser');
-        var app = document.getElementById('app');
-
-        app.style.display = 'none';
-        browser.style.display = 'block';
-
-        browser.addEventListener('mozbrowserlocationchange', function(event){
-            if(event.detail.indexOf('http://alvinsj.com') == 0){
-                var url = new URL(event.detail);
-                browser.stop();
-                browser.style.display = 'none';
-                app.style.display = 'block';
-                AppDispatcher.dispatch({
-                    type: ActionTypes.INSTAGRAM_CODE,
-                    code: url.search.slice(6)
-                });
-            }
-        });
-        browser.setAttribute('src', auth_url);
-    },
-    getInstagramAccessToken: function(code){
-        console.log('getInstagramAccessToken')
-        var code = StateStore.getInstagramCode();
-
-        ig.authorize_user(code, _redirect_url, function(err, result){
-            if (err) {
-                console.log('error: ', err);
-            } else {
-                ig.use({ access_token: result.access_token });
-                AppDispatcher.dispatch({
-                    type: ActionTypes.INSTAGRAM_ACCESS_TOKEN,
-                    access_token: result.access_token
-                })
-            }
-        });
-    },
-    fetchTimeline: function(){
-        console.log('fetchTimeline');
-        var access_token = StateStore.getInstagramAccessToken();
-
-       localForage.getItem('timeline', function(err, medias) {
-               if(err) console.log(err);
-            if(medias) {
-                AppDispatcher.dispatch({
-                    type: ActionTypes.FETCH_TIMELINE,
-                    medias: medias
-                })
-            }
-       });
-       ig.user_self_feed(function(err, medias, pagination, remaining, limit) {
-              if(err) console.log(err);
-
-           AppDispatcher.dispatch({
-                type: ActionTypes.FETCH_TIMELINE,
-                medias: medias
-            });
-           localForage.setItem('timeline', medias, function() {});
-       });
-
-    },
+    // getInstagramCode: function(){
+    //     console.log('getInstagramCode')
+    //
+    //     // StateStore.ig = require('instagram-node').instagram();
+    //     // ig = StateStore.ig;
+    //     // ig.use(require('instagram'));
+    //
+    //     var auth_url = ig.get_authorization_url(_redirect_url);
+    //     var browser = document.getElementById('browser');
+    //     var app = document.getElementById('app');
+    //
+    //     app.style.display = 'none';
+    //     browser.style.display = 'block';
+    //
+    //     browser.addEventListener('mozbrowserlocationchange', function(event){
+    //         if(event.detail.indexOf('http://alvinsj.com') == 0){
+    //             var url = new URL(event.detail);
+    //             browser.stop();
+    //             browser.style.display = 'none';
+    //             app.style.display = 'block';
+    //             AppDispatcher.dispatch({
+    //                 type: ActionTypes.INSTAGRAM_CODE,
+    //                 code: url.search.slice(6)
+    //             });
+    //         }
+    //     });
+    //     browser.setAttribute('src', auth_url);
+    // },
+    // getInstagramAccessToken: function(code){
+    //     console.log('getInstagramAccessToken')
+    //     var code = StateStore.getInstagramCode();
+    //
+    //     ig.authorize_user(code, _redirect_url, function(err, result){
+    //         if (err) {
+    //             console.log('error: ', err);
+    //         } else {
+    //             ig.use({ access_token: result.access_token });
+    //             AppDispatcher.dispatch({
+    //                 type: ActionTypes.INSTAGRAM_ACCESS_TOKEN,
+    //                 access_token: result.access_token
+    //             })
+    //         }
+    //     });
+    // },
+    // fetchTimeline: function(){
+    //     console.log('fetchTimeline');
+    //     var access_token = StateStore.getInstagramAccessToken();
+    //
+    //    localForage.getItem('timeline', function(err, medias) {
+    //            if(err) console.log(err);
+    //         if(medias) {
+    //             AppDispatcher.dispatch({
+    //                 type: ActionTypes.FETCH_TIMELINE,
+    //                 medias: medias
+    //             })
+    //         }
+    //    });
+    //    ig.user_self_feed(function(err, medias, pagination, remaining, limit) {
+    //           if(err) console.log(err);
+    //
+    //        AppDispatcher.dispatch({
+    //             type: ActionTypes.FETCH_TIMELINE,
+    //             medias: medias
+    //         });
+    //        localForage.setItem('timeline', medias, function() {});
+    //    });
+    //
+    // },
     fetchUserTimeline: function(user){
         console.log('fetchUserTimeline', user);
 
--- js/stores/state-store.js
+++ js/stores/state-store.js
@@ -1,59 +1,55 @@
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
-var ig = require('instagram-node').instagram();
-    ig.use(require('instagram'));
 
 var AppDispatcher = require('dispatchers/app-dispatcher');
 var ActionTypes = require('constants/app-constants').ActionTypes;
 var _access_token = null, _code = null;
 
 var StateStore = assign({}, EventEmitter.prototype, {
-	getInstagramCode: function(){
-		return _code;
-	},
-	getInstagramAccessToken: function(){
-		return _access_token;
-	},
-	dispatchToken: AppDispatcher.register(function(action){				
-		switch (action.type) {
-			case ActionTypes.INSTAGRAM_CODE: 
-				_code = action.code;
-				StateStore.emit(ActionTypes.INSTAGRAM_CODE, action.code);
-				break;
+    getInstagramCode: function(){
+        return _code;
+    },
+    getInstagramAccessToken: function(){
+        return _access_token;
+    },
+    dispatchToken: AppDispatcher.register(function(action){
+        switch (action.type) {
+            case ActionTypes.INSTAGRAM_CODE:
+                _code = action.code;
+                StateStore.emit(ActionTypes.INSTAGRAM_CODE, action.code);
+                break;
 
-			case ActionTypes.INSTAGRAM_ACCESS_TOKEN:
-				_access_token = action.access_token;
-				StateStore.emit(ActionTypes.INSTAGRAM_ACCESS_TOKEN, action.access_token);
-				break;
-			
-			case ActionTypes.VIEW_DETAILS:
-		      	StateStore.emit(ActionTypes.VIEW_DETAILS, action.media);
-		      	break;
+            case ActionTypes.INSTAGRAM_ACCESS_TOKEN:
+                _access_token = action.access_token;
+                StateStore.emit(ActionTypes.INSTAGRAM_ACCESS_TOKEN, action.access_token);
+                break;
 
-		    case ActionTypes.VIEW_USER:
-		      	StateStore.emit(ActionTypes.VIEW_USER, action.user);
-		      	break;
+            case ActionTypes.VIEW_DETAILS:
+                  StateStore.emit(ActionTypes.VIEW_DETAILS, action.media);
+                  break;
 
-		    case ActionTypes.BACK_TO_HOME:
-		      	StateStore.emit(ActionTypes.BACK_TO_HOME);
-		      	break;
+            case ActionTypes.VIEW_USER:
+                  StateStore.emit(ActionTypes.VIEW_USER, action.user);
+                  break;
 
-		    case ActionTypes.VIEW_SETTINGS:
-		    	StateStore.emit(ActionTypes.VIEW_SETTINGS);
-		    	break;
+            case ActionTypes.BACK_TO_HOME:
+                  StateStore.emit(ActionTypes.BACK_TO_HOME);
+                  break;
 
-		    case ActionTypes.LOGOUT:
-		    	_code = null;
-		    	_access_token = null;
-		    	StateStore.emit(ActionTypes.LOGOUT);
-		    	break;
+            case ActionTypes.VIEW_SETTINGS:
+                StateStore.emit(ActionTypes.VIEW_SETTINGS);
+                break;
 
-		    default:
-		      // do nothing
-		  }
-	})
+            case ActionTypes.LOGOUT:
+                _code = null;
+                _access_token = null;
+                StateStore.emit(ActionTypes.LOGOUT);
+                break;
+
+            default:
+              // do nothing
+          }
+    })
 });
-
-StateStore.ig = ig;
 
 module.exports = StateStore;
